6f4ec380e6d077679264ce8f074f6b2e1cc893bd
Ishwar Baisla <68193554+ishwar2303@users.noreply.github.com>
2022-02-01 17:39:02 +0530

    Update DatabaseUsers.sql

--- DatabaseUsers.sql
+++ DatabaseUsers.sql
@@ -1,5 +1,15 @@
+/* Admin */
 CREATE USER 'admin'@'localhost' IDENTIFIED BY '23031999';
 
 SHOW GRANTS FOR 'admin'@'localhost';
 
 GRANT ALL PRIVILEGES ON `quizwit`.`Admin` TO 'admin'@'localhost';
+
+
+
+/* Student */
+CREATE USER 'student'@'localhost' IDENTIFIED BY '23031999';
+
+SHOW GRANTS FOR 'student'@'localhost';
+
+GRANT ALL PRIVILEGES ON `quizwit`.`student` TO 'student'@'localhost';
