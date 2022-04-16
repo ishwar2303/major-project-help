e13394f0161a6175b47e2696de60f2c7b89b865b
Ishwar Baisla <68193554+ishwar2303@users.noreply.github.com>
2022-04-16 18:08:53 +0530

    Update Database-Users.sql

--- Database-Users.sql
+++ Database-Users.sql
@@ -28,3 +28,4 @@ GRANT ALL PRIVILEGES ON `quizwit`.`Students` TO 'student'@'localhost';
 GRANT ALL PRIVILEGES ON `quizwit`.`Attempts` TO 'student'@'localhost';
 GRANT ALL PRIVILEGES ON `quizwit`.`SectionNavigation` TO 'student'@'localhost';
 GRANT ALL PRIVILEGES ON `quizwit`.`QuestionNavigation` TO 'student'@'localhost';
+GRANT SELECT ON `quizwit`.`Questions` TO 'student'@'localhost';
